e5831a10d007b9f52b1e41052dfc9b9e55ebf24d
pavel <p.kruchkovskij@aspex.kz>
2023-08-24 01:57:36 +0500

    add cert

--- main.py
+++ main.py
@@ -119,5 +119,7 @@ async def say_hello(name: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=80, log_level="info", reload=True
+    uvicorn.run("main:app", host='0.0.0.0', port=80, log_level="info", reload=True, ssl_keyfile="./key.pem",
+                ssl_certfile="./cert.pem"
+
 )
